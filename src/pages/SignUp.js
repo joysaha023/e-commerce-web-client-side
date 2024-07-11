4e8ac365a3c17078d18d34c58d972f4105bc042a
Joy <github.joysaha@gmail.com>
2024-07-11 12:20:22 +0600

    signup page

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,11 +1,124 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  return (
-    <div>
-      Sign up
-    </div>
-  )
-}
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+    name: "",
+    confirmedPassword: "",
+    profilepic: "",
+  });
 
-export default SignUp
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setData((preve) => {
+      return {
+        ...preve,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section id="signup">
+      <div className="mx-auto container p-4">
+        <div className="bg-white max-w-sm py-5 mx-auto p-5 w-full">
+          <div className="w-20 h-20  mx-auto">
+            <img src="https://i.ibb.co/H2Mbg8y/signin.gif" alt="icon" />
+          </div>
+
+          <form className="pt-6" onSubmit={handleSubmit}>
+            <div className="grid">
+              <label>Name : </label>
+              <div className="bg-slate-100 p-2">
+                <input
+                  type="text"
+                  placeholder="enter your name"
+                  name="name"
+                  value={data.name}
+                  onChange={handleOnChange}
+                  className="w-full h-full outline-none bg-transparent"
+                />
+              </div>
+            </div>
+            <div className="grid">
+              <label>Email: </label>
+              <div className="bg-slate-100 p-2">
+                <input
+                  type="email"
+                  placeholder="enter email"
+                  name="email"
+                  value={data.email}
+                  onChange={handleOnChange}
+                  className="w-full h-full outline-none bg-transparent"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label>Password: </label>
+              <div className="bg-slate-100 p-2 flex">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="enter password"
+                  name="password"
+                  value={data.password}
+                  onChange={handleOnChange}
+                  className="w-full h-full outline-none bg-transparent"
+                />
+                <div
+                  className="cursor-pointer text-xl"
+                  onClick={() => setShowPassword((preve) => !preve)}
+                >
+                  <span>{showPassword ? <FaEyeSlash /> : <FaEye />}</span>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <label>Confirm Password: </label>
+              <div className="bg-slate-100 p-2 flex">
+                <input
+                  type={showConfirmPassword ? "text" : "password"}
+                  placeholder="enter confirmed password"
+                  name="confirmedPassword"
+                  value={data.confirmedPassword}
+                  onChange={handleOnChange}
+                  className="w-full h-full outline-none bg-transparent"
+                />
+                <div
+                  className="cursor-pointer text-xl"
+                  onClick={() => setShowConfirmPassword((preve) => !preve)}
+                >
+                  <span>{showConfirmPassword ? <FaEyeSlash /> : <FaEye />}</span>
+                </div>
+              </div>
+            </div>
+            <button className="bg-red-600 hover:bg-red-700 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6">
+              Sign Up
+            </button>
+          </form>
+          <p className="my-4">
+            Already have an account ?{" "}
+            <Link
+              to={"/login"}
+              className="text-red-600 hover:underline hover:text-red-700"
+            >
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SignUp;
