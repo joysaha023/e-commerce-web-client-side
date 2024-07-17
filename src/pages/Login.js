b9e2625ac25e6872310b1e0c1ede8f579c1b43f3
Joy <github.joysaha@gmail.com>
2024-07-17 10:29:53 +0600

    image upload from file

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,34 +6,34 @@ const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [data, setData] = useState({
     email: "",
-    password: ""
-  })
+    password: "",
+  });
 
-  const handleOnChange = (e) =>{
-    const {name, value} = e.target
-    setData((preve)=> {
-      return{
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setData((preve) => {
+      return {
         ...preve,
-        [name] : value
-      }
-    })
-  }
+        [name]: value,
+      };
+    });
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+  };
 
-  console.log("data login", data)
+  console.log("data login", data);
 
   return (
     <section id="login">
       <div className="mx-auto container p-4">
         <div className="bg-white max-w-sm py-5 mx-auto p-5 w-full">
-          <div className="w-20 h-20  mx-auto">
+          <div className="w-20 h-20  mx-auto  ">
             <img src="https://i.ibb.co/H2Mbg8y/signin.gif" alt="icon" />
           </div>
 
-          <form className="pt-6" onSubmit={handleSubmit}>
+          <form className="pt-6 flex flex-col gap-2" onSubmit={handleSubmit}>
             <div className="grid">
               <label>Email: </label>
               <div className="bg-slate-100 p-2">
@@ -59,15 +59,34 @@ const Login = () => {
                   onChange={handleOnChange}
                   className="w-full h-full outline-none bg-transparent"
                 />
-                <div className="cursor-pointer text-xl" onClick={()=> setShowPassword((preve)=> !preve)}>
+                <div
+                  className="cursor-pointer text-xl"
+                  onClick={() => setShowPassword((preve) => !preve)}
+                >
                   <span>{showPassword ? <FaEyeSlash /> : <FaEye />}</span>
                 </div>
               </div>
-              <Link to={'/forgot-password'} className="block w-fit ml-auto hover:underline hover:text-red-600"> Forget Password?</Link>
+              <Link
+                to={"/forgot-password"}
+                className="block w-fit ml-auto hover:underline hover:text-red-600"
+              >
+                {" "}
+                Forget Password?
+              </Link>
             </div>
-            <button className="bg-red-600 hover:bg-red-700 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6">Login</button>
+            <button className="bg-red-600 hover:bg-red-700 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6">
+              Login
+            </button>
           </form>
-          <p className="my-4">Don't have account ? <Link to={"/signup"} className="text-red-600 hover:underline hover:text-red-700">Sign up</Link></p>
+          <p className="my-4">
+            Don't have account ?{" "}
+            <Link
+              to={"/signup"}
+              className="text-red-600 hover:underline hover:text-red-700"
+            >
+              Sign up
+            </Link>
+          </p>
         </div>
       </div>
     </section>
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -23,6 +23,12 @@ const SignUp = () => {
     });
   };
 
+  const handleUploadPic = (e) => {
+    const file = e.target.files[0]
+
+    console.log("file", file)
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
@@ -31,11 +37,21 @@ const SignUp = () => {
     <section id="signup">
       <div className="mx-auto container p-4">
         <div className="bg-white max-w-sm py-5 mx-auto p-5 w-full">
-          <div className="w-20 h-20  mx-auto">
-            <img src="https://i.ibb.co/H2Mbg8y/signin.gif" alt="icon" />
+          <div className="w-20 h-20  mx-auto relative overflow-hidden rounded-full">
+            <div>
+              <img src="https://i.ibb.co/H2Mbg8y/signin.gif" alt="icon" />
+            </div>
+            <form>
+              <label>
+                <div className="text-xs bg-opacity-80 pb-4 pt-2 cursor-pointer bg-slate-200 py-4 text-center absolute bottom-0 w-full">
+                  Upload Photo
+                </div>
+                <input type="file" className="hidden"  onChange={handleUploadPic}/>
+              </label>
+            </form>
           </div>
 
-          <form className="pt-6" onSubmit={handleSubmit}>
+          <form className="pt-6 flex flex-col gap-2" onSubmit={handleSubmit}>
             <div className="grid">
               <label>Name : </label>
               <div className="bg-slate-100 p-2">
@@ -98,7 +114,9 @@ const SignUp = () => {
                   className="cursor-pointer text-xl"
                   onClick={() => setShowConfirmPassword((preve) => !preve)}
                 >
-                  <span>{showConfirmPassword ? <FaEyeSlash /> : <FaEye />}</span>
+                  <span>
+                    {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+                  </span>
                 </div>
               </div>
             </div>
